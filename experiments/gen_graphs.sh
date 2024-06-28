630db47c4f32804b732b6769472179e7a90421ed
BarrySlyDelgado <barryjsly@gmail.com>
2024-06-27 23:07:31 -0400

    add gen graphs

---
+++ experiments/gen_graphs.sh
@@ -0,0 +1,8 @@
+python plotting_tools/plot_fig_7.py
+python plotting_tools/plot_fig_11_a.py
+python plotting_tools/plot_fig_11_b.py
+python plotting_tools/plot_fig_12.py
+python plotting_tools/plot_fig_13.py
+python plotting_tools/plot_fig_14_apps.py
+python plotting_tools/plot_fig_14_dv3.py
+python plotting_tools/plot_fig_15.py
