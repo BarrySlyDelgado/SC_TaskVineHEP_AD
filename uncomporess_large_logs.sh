8c4a48a05142a788f62604e722c1daa20374ca4c
BarrySlyDelgado <barryjsly@gmail.com>
2024-06-27 23:03:07 -0400

    gzip script

---
+++ uncomporess_large_logs.sh
@@ -0,0 +1,3 @@
+gzip -dk paper_figures/figure_7_data_transfer_comparison/logs/tv_debug.gz
+gzip -dk paper_figures/figure_7_data_transfer_comparison/logs/wq_debug.gz
+gzip -dk paper_figures/figure_15_dv3_huge/logs/tr.gz
